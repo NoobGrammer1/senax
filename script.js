64e729069c32e404172f63b97a1ff6c97bebe729
Anthony Alexander Chamba Elizalde <anthony.chamba@epn.edu.ec>
2023-02-24 12:09:31 -0500

    Semi functional page achieved

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const cedulaRadio = document.querySelector('input[value="cedula"]');
 const inputApellido = document.getElementById('input-apellido');
 const inputCedula = document.getElementById('input-cedula');
 
+
 inputApellido.style.display = 'inline-block';
 inputCedula.style.display = 'none';
 
@@ -24,5 +25,43 @@ cedulaRadio.addEventListener('change', () => {
 });
 
 
+const form = document.querySelector('form');
+const resultsContainer = document.getElementById('results-container');
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // Evita el comportamiento predeterminado del formulario
+
+  // Mostrar el contenedor de resultados
+  resultsContainer.style.display = 'block';
+
+  // Colocar el foco en el contenedor de resultados
+  resultsContainer.focus();
+  resultsContainer.scrollIntoView({ behavior: 'smooth' });
+});
+
+const verInformacionLinks = document.querySelectorAll('.ver-informacion');
+const moreDetailsContainer = document.getElementById('more-details');
+
+verInformacionLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    
+    // Ocultar el contenedor de resultados
+    resultsContainer.style.display = 'none';
+    
+    // Mostrar el contenedor de más detalles
+    moreDetailsContainer.style.display = 'block';
+    
+    // Enfocar el contenedor de información personal
+    const personalInfoContainer = document.getElementById('personal-info');
+    personalInfoContainer.focus();
+    
+    // Hacer que la pantalla se desplace al contenedor de información personal
+    personalInfoContainer.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+
+
+// Path: index.html
 
